87dd69334c04a5d65c37c91514f79f3b9fe8696d
Roastem <caplistiorimp1ng123@gmail.com>
2023-07-25 15:31:36 +0530

    weather api

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin: "*",
 }));
 
-// Define the route to handle the API request
+// Define the route to handle the chatbot API request
 app.get('/chatbot/api', async (req, res) => {
   const { uid, msg } = req.query;
 
@@ -23,17 +23,42 @@ app.get('/chatbot/api', async (req, res) => {
   const apiLink = `http://api.brainshop.ai/get?bid=${bid}&key=${apiKey}&uid=${uid}&msg=${msg}`;
 
   try {
-    // Make the API request using Axios
+    // Make the chatbot API request using Axios
     const response = await axios.get(apiLink);
 
-    // Get the API response data
+    // Get the chatbot API response data
     const responseData = response.data;
 
-    // Send the API response data back to the user
+    // Send the chatbot API response data back to the user
     res.json(responseData);
   } catch (error) {
     // Handle errors, if any
-    res.status(500).json({ error: 'An error occurred while fetching the data.' });
+    res.status(500).json({ error: 'An error occurred while fetching the chatbot data.' });
+  }
+});
+
+// Define the route to handle the weather API request
+app.get('/utils/weather/api', async (req, res) => {
+  const { location } = req.query;
+
+  // Weather API base link and API key
+  const weatherApiBaseLink = 'https://api.weatherapi.com/v1/current.json';
+  const weatherApiKey = process.env.weatherApiKey;
+
+  const weatherApiLink = `${weatherApiBaseLink}?key=${weatherApiKey}&q=${location}&aqi=yes`;
+
+  try {
+    // Make the weather API request using Axios
+    const response = await axios.get(weatherApiLink);
+
+    // Get the weather API response data
+    const responseData = response.data;
+
+    // Send the weather API response data back to the user
+    res.json(responseData);
+  } catch (error) {
+    // Handle errors, if any
+    res.status(500).json({ error: 'An error occurred while fetching the weather data.' });
   }
 });
 

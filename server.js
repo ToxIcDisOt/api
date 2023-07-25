b982822fe753105d2a2118cce162fa8c86ec7133
Roastem <caplistiorimp1ng123@gmail.com>
2023-07-25 15:41:59 +0530

    .

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const dotenv = require('dotenv');
-const cors = require('cors'); // Add the cors module
+const cors = require('cors');
 const app = express();
 
 // Load environment variables from .env file
@@ -12,7 +12,7 @@ app.use(cors({
   origin: "*",
 }));
 
-// Define the route to handle the chatbot API request
+// Define the route to handle the API request for the chatbot
 app.get('/chatbot/api', async (req, res) => {
   const { uid, msg } = req.query;
 
@@ -23,42 +23,17 @@ app.get('/chatbot/api', async (req, res) => {
   const apiLink = `http://api.brainshop.ai/get?bid=${bid}&key=${apiKey}&uid=${uid}&msg=${msg}`;
 
   try {
-    // Make the chatbot API request using Axios
+    // Make the API request using Axios
     const response = await axios.get(apiLink);
 
-    // Get the chatbot API response data
+    // Get the API response data
     const responseData = response.data;
 
-    // Send the chatbot API response data back to the user
+    // Send the API response data back to the user
     res.json(responseData);
   } catch (error) {
     // Handle errors, if any
-    res.status(500).json({ error: 'An error occurred while fetching the chatbot data.' });
-  }
-});
-
-// Define the route to handle the weather API request
-app.get('/utils/weather/api', async (req, res) => {
-  const { location } = req.query;
-
-  // Weather API base link and API key
-  const weatherApiBaseLink = 'https://api.weatherapi.com/v1/current.json';
-  const weatherApiKey = process.env.weatherApiKey;
-
-  const weatherApiLink = `${weatherApiBaseLink}?key=${weatherApiKey}&q=${location}&aqi=yes`;
-
-  try {
-    // Make the weather API request using Axios
-    const response = await axios.get(weatherApiLink);
-
-    // Get the weather API response data
-    const responseData = response.data;
-
-    // Send the weather API response data back to the user
-    res.json(responseData);
-  } catch (error) {
-    // Handle errors, if any
-    res.status(500).json({ error: 'An error occurred while fetching the weather data.' });
+    res.status(500).json({ error: 'An error occurred while fetching the data.' });
   }
 });
 
